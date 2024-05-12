80879f3d4aef352e8966c21263e3fdebe041bf0a
Isakovic <di461643@win.rz.rwth-aachen.de>
2024-05-12 19:53:06 +0200

    add v2

---
+++ ihk24_v1/ihk24_v1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ihk24_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
